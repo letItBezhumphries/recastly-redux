18dc8ee2bb147a91ee2b57b9ea8c369a3d5ecfea
EricZH <letItBezhumphries>
2018-11-29 19:42:40 -0800

    added react-test-renderer

--- src/components/App.js
+++ src/components/App.js
@@ -23,8 +23,6 @@ export default class App extends React.Component {
 
   componentDidMount() {
     store.dispatch(handleVideoSearch('radiant children'));
-
-
   }
 
   handleVideoListEntryTitleClick(video) {
